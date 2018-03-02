4e5f471a7c599993f35a44986dfb0b023a6976a7
Leo McArdle <leo.mcardle.17@ucl.ac.uk>
2018-03-02 13:52:12 +0000

    tmp: add admin form

--- ssig_site/auth/admin.py
+++ ssig_site/auth/admin.py
@@ -1,3 +1,19 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
-# Register your models here.
+from .models import User
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    def clean_password(self):
+        return self.initial['password']
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    form = UserChangeForm
+
+    list_display = ('upi', 'email', 'full_name', 'department')
+    list_filter = ('is_superuser',)
